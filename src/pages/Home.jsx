2a3f1458280ecdc34efd6d98e5275a33886f9817
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-03-13 10:18:14 +0200

    make api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 import { useRecoilValue } from "recoil";
+
+import { getProducts } from "@api";
 import { userState, getFullName } from "@store";
 
 function Home() {
     const fullName = useRecoilValue(getFullName);
+
+    useEffect(() => {
+        getProducts().then(response => console.log(response));
+    }, []);
 
     return (
         <div>
