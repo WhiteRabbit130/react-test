04cbdd9f227ce6ed776062c3a65b5c80f55757bf
smith525525 <smith1989525@gmail.com>
2024-03-15 16:58:52 +0200

    make logo

---
+++ src/components/login/LoginSide.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+function LoginSide() {
+    return (
+        <div className="login-side">
+
+        </div>
+    )
+}
+
+export default LoginSide;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,11 @@ import { useRecoilState, useRecoilValue } from "recoil";
 import { Navigate, useNavigate } from "react-router-dom";
 
 import { userState } from "@store";
+import Logo from "@components/login/Logo";
+import LogoSide from "@components/login/LogoSide";
+import LoginSide from "@components/login/LoginSide";
+
+import '@components/login/style.css';
 
 function Login() {
     const [user, setUser] = useRecoilState(userState);
@@ -20,9 +25,11 @@ function Login() {
     };
 
     return (
-        <div>
-            Login
-            <button onClick={onLogin}>Sign In</button>
+        <div className="login">
+            <Logo />
+            <LogoSide />
+            <LoginSide />
+            {/* <button onClick={onLogin}>Sign In</button> */}
         </div>
     )
 }
