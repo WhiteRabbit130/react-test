f3dcc308574860fab82e972769c2b2fc0602bd20
WhiteRbbit130 <154484747+WhiteRabbit130@users.noreply.github.com>
2024-03-18 19:40:31 +0200

    update button

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client'
 
 import '@styles/index.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
+import reportWebVitals from './test/reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
