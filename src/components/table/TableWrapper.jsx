e05409814b5d9bd228010c6210d2c254177c250c
WhiteRbbit130 <154484747+WhiteRabbit130@users.noreply.github.com>
2024-03-19 03:34:15 +0200

    update table

--- src/components/table/TableWrapper.jsx
+++ src/components/table/TableWrapper.jsx
@@ -2,47 +2,49 @@ import { Box, Table, TableBody, TableContainer, TableHead, TableRow, Paper } fro
 import { StringCellRenderer } from './TableCellRenderer'
 
 function TableWrapper ({ headers, data }) {
-    return (
-        <TableContainer component={Paper}>
-            <Table className='table'>
-                <TableHead>
-                    <TableRow className='thead'>
-                    {headers.map(header => (
-                        <StringCellRenderer
-                        key={header.dataKey}
-                        value={header.label}
-                        mediaType={header.mediaType}
-                        />
-                    ))}
-                    </TableRow>
-                </TableHead>
-
-                <TableBody>
-                    {data.length == 0 ? (                    
-                        <Box display='flex' justifyContent='center'>
-                            No Item to display
-                        </Box>
-                    ) : (
-                        data.map((row, index) => (
-                            <TableRow key={index}>
-                            {headers.map(header => {
-                                const value = row[header.dataKey]
-                                const CellRenderer = header.renderer || StringCellRenderer
-
-                                return (
-                                <CellRenderer
-                                    key={header.dataKey}
-                                    value={value}
-                                    mediaType={header.mediaType}
+    return (  
+        <>      
+            {data.length == 0 ? (                    
+                <Box display='flex' justifyContent='center'>
+                    No Item to display
+                </Box>
+            ) : (
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                            {headers.map(header => (
+                                <StringCellRenderer
+                                key={header.dataKey}
+                                value={header.label}
+                                mediaType={header.mediaType}
                                 />
-                                )
-                            })}
+                            ))}
                             </TableRow>
-                        ))
-                    )}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                        </TableHead>
+
+                        <TableBody>
+                            {data.map((row, index) => (
+                                <TableRow key={index}>
+                                    {headers.map(header => {
+                                        const value = row[header.dataKey]
+                                        const CellRenderer = header.renderer || StringCellRenderer
+
+                                        return (
+                                        <CellRenderer
+                                            key={header.dataKey}
+                                            value={value}
+                                            mediaType={header.mediaType}
+                                        />
+                                        )
+                                    })}
+                                </TableRow>
+                            ))}                    
+                        </TableBody>
+                    </Table>
+                </TableContainer> 
+            )}
+        </>
     )
 }
 
